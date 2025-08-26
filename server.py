360e6a35890f87066f58e8e36d6025b8682a9ca0
thienan216 <pnta2165@gmail.com>
2025-08-26 12:38:56 +0700

    them file server.py

---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+from _thread import *
+import pickle
+from game import Game
+
+server = "10.11.250.207"
+port = 5555
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen()
+print("Waiting for a connection, Server Started")
+
+connected = set()
+games = {}
+idCount = 0
+
+def threaded_client(conn, p, gameId):
+    pass
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    idCount += 1
+    p = 0
+    gameId = (idCount - 1) // 2
+    if idCount % 2 == 1:
+        games[gameId] = Game(gameId)
+        print("Creating a new game...")
+    else:
+        games[gameId].ready = True
+        p = 1
+        
+    start_new_thread(threaded_client, (conn))
