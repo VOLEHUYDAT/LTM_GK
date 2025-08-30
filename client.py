344c6488ccf6fa8cc0ef259dfdb3dca5e1698b2a
VOLEHUYDAT <huydat13825@gmail.com>
2025-08-30 17:02:59 +0700

    tạo client và requirements.txt

---
+++ client.py
@@ -0,0 +1,22 @@
+import pygame   
+from network import Network 
+import pickle   
+pygame.font.init()  
+
+width = 700
+height = 700
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+
+
+def redrawWindow(win, game, p):
+    win.fill((128,128,128))
+    pass
+
+
+def main():
+    pass
+
+
+main() 
